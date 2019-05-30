c52023c218c15b6a8475a47e3988ed3cbc80df04
jonathon <jonathonkindle@gmail.com>
2019-05-30 16:18:09 -0700

    first commit in new file structure

---
+++ env02/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jonathon\desktop\schoolclasses\itc172\172final\env02\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
